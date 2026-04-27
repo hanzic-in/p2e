fec69494e888439a43af65cd972ab029c6b53689
Han <rizkyupit40@gmail.com>
2026-04-27 18:21:52 +0700

    Add AdBannerCarousel to dashboard view

--- lib/features/dashboard/dashboard_view.dart
+++ lib/features/dashboard/dashboard_view.dart
@@ -17,6 +17,7 @@ class DashboardView extends StatelessWidget {
         child: Column(
           children: [
             TycoonHeader(bCoin: prov.bCoin, keyCoin: prov.keyCoin, special: prov.special),
+            const AdBannerCarousel(),
             Expanded(
               child: ListView.builder(
                 padding: const EdgeInsets.all(20),
