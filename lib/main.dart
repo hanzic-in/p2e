074445759f4dbbd20d24edbc7f2981ddffc86676
Han <rizkyupit40@gmail.com>
2026-04-27 10:44:10 +0700

    Refactor main.dart by removing MainNavigation
    Removed MainNavigation class and its related code.

--- lib/main.dart
+++ lib/main.dart
@@ -1,8 +1,8 @@
 import 'package:flutter/material.dart';
 import 'package:provider/provider.dart';
 import './features/dashboard/provider/dashboard_provider.dart';
-import './features/dashboard/dashboard_view.dart';
 import './core/constants/app_colors.dart';
+import './app.dart';
 
 void main() {
   runApp(
@@ -20,46 +20,11 @@ class MyApp extends StatelessWidget {
   Widget build(BuildContext context) {
     return MaterialApp(
       debugShowCheckedModeBanner: false,
+      title: 'Bullish Farm',
       theme: ThemeData.dark().copyWith(
         scaffoldBackgroundColor: AppColors.darkBg,
       ),
       home: MainNavigation(),
-    );
-  }
-}
-
-// KONTROLER NAVIGASI
-class MainNavigation extends StatefulWidget {
-  @override
-  _MainNavigationState createState() => _MainNavigationState();
-}
-
-class _MainNavigationState extends State<MainNavigation> {
-  int _currentIndex = 0;
-
-  final List<Widget> _pages = [
-    DashboardView(),
-    Center(child: Text("INDUSTRIAL MISSION", style: TextStyle(color: Colors.white))),
-    Center(child: Text("CITY WALLET", style: TextStyle(color: Colors.white))),
-  ];
-
-  @override
-  Widget build(BuildContext context) {
-    return Scaffold(
-      body: _pages[_currentIndex],
-      bottomNavigationBar: BottomNavigationBar(
-        currentIndex: _currentIndex,
-        onTap: (index) => setState(() => _currentIndex = index),
-        backgroundColor: AppColors.cardBg,
-        selectedItemColor: AppColors.primaryNeon,
-        unselectedItemColor: AppColors.textGray,
-        type: BottomNavigationBarType.fixed,
-        items: [
-          BottomNavigationBarItem(icon: Icon(Icons.location_city), label: "City"),
-          BottomNavigationBarItem(icon: Icon(Icons.assignment), label: "Mission"),
-          BottomNavigationBarItem(icon: Icon(Icons.account_balance_wallet), label: "Wallet"),
-        ],
-      ),
     );
   }
 }
