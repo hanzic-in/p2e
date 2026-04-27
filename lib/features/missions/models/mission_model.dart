782a6f1ea7d63f2311c9ffe958cb75e88f15a71a
Han <rizkyupit40@gmail.com>
2026-04-27 16:19:35 +0700

    Update mission_model.dart

--- lib/features/missions/models/mission_model.dart
+++ lib/features/missions/models/mission_model.dart
@@ -1,13 +1,35 @@
+class MissionStep {
+  final int order;
+  final String title;
+  final String description;
+  final int reward;
+  final String type;
+  final String? exampleImageUrl;
+
+  MissionStep({
+    required this.order,
+    required this.title,
+    required this.description,
+    required this.reward,
+    required this.type,
+    this.exampleImageUrl,
+  });
+}
+
 class Mission {
+  final String id;
   final String title;
   final String logoUrl;
   final String bannerUrl;
-  final int reward;
+  final int totalReward;
+  final List<MissionStep> steps;
 
   Mission({
+    required this.id,
     required this.title,
     required this.logoUrl,
     required this.bannerUrl,
-    required this.reward,
+    required this.totalReward,
+    required this.steps,
   });
 }
