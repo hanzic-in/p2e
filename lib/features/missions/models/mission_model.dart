faa5a0a9e2f9cf128e758158065d7b767f4690f5
Han <rizkyupit40@gmail.com>
2026-04-27 15:22:00 +0700

    Add Mission model with properties and constructor

--- lib/features/missions/models/mission_model.dart
+++ lib/features/missions/models/mission_model.dart
@@ -1,1 +1,13 @@
+class Mission {
+  final String title;
+  final String logoUrl;
+  final String bannerUrl;
+  final int reward;
 
+  Mission({
+    required this.title,
+    required this.logoUrl,
+    required this.bannerUrl,
+    required this.reward,
+  });
+}
