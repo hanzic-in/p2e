107717348bd2dd9f92cad97287e6e9ab9efca476
Han <rizkyupit40@gmail.com>
2026-04-27 11:19:10 +0700

    Refactor DashboardProvider to manage multiple balances

--- lib/features/dashboard/provider/dashboard_provider.dart
+++ lib/features/dashboard/provider/dashboard_provider.dart
@@ -3,40 +3,52 @@ import 'package:flutter/material.dart';
 import '../models/farm_item_model.dart';
 
 class DashboardProvider extends ChangeNotifier {
-  double _balance = 0.0;
-  List<FarmItem> _myFarms = initialFarmData;
+  double _balanceBCoin = 0.0;
+  double _balanceKey = 0.0;
+  double _balanceSpecial = 0.0;
+  List<FarmItem> _myFarms = initialFarms;
+  final Map<int, Timer?> _timers = {};
 
-  double get balance => _balance;
+  double get bCoin => _balanceBCoin;
+  double get key => _balanceKey;
+  double get special => _balanceSpecial;
   List<FarmItem> get myFarms => _myFarms;
 
-  DashboardProvider() {
-    _startFarmProduction();
-  }
+  void startProduction(int index) {
+    var farm = _myFarms[index];
+    if (farm.status != ProductionStatus.idle) return;
 
-  void _startFarmProduction() {
-    Timer.periodic(const Duration(seconds: 1), (timer) {
-      for (var farm in _myFarms) {
-        if (!farm.isLocked) {
-          _balance += (farm.incomePerCycle / farm.cycleDuration);
-        }
+    farm.status = ProductionStatus.producing;
+    farm.remainingSeconds = farm.cycleDuration;
+    notifyListeners();
+
+    _timers[farm.id] = Timer.periodic(const Duration(seconds: 1), (timer) {
+      if (farm.remainingSeconds > 0) {
+        farm.remainingSeconds--;
+        notifyListeners();
+      } else {
+        farm.status = ProductionStatus.ready;
+        timer.cancel();
+        notifyListeners();
       }
-      notifyListeners();
     });
   }
 
-  void unlockFarm(int index) {
-    if (_balance >= _myFarms[index].unlockPrice) {
-      _balance -= _myFarms[index].unlockPrice;
-      var item = _myFarms[index];
-      _myFarms[index] = FarmItem(
-        name: item.name,
-        productionLabel: item.productionLabel,
-        incomePerCycle: item.incomePerCycle,
-        cycleDuration: item.cycleDuration,
-        isLocked: false,
-        icon: item.icon,
-      );
-      notifyListeners();
+  void claimResult(int index) {
+    var farm = _myFarms[index];
+    if (farm.status != ProductionStatus.ready) return;
+
+    _balanceBCoin += farm.incomeBCoin;
+    _balanceKey += farm.incomeKey;
+    farm.status = ProductionStatus.idle;
+    notifyListeners();
+  }
+
+  @override
+  void dispose() {
+    for (var timer in _timers.values) {
+      timer?.cancel();
     }
+    super.dispose();
   }
 }
