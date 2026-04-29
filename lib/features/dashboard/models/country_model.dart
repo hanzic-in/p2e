0f316e77e9a7804d7a3cc1bca83bf85344031e8a
Han <rizkyupit40@gmail.com>
2026-04-29 13:14:11 +0700

    Add CountryOrder model with continent enumeration

---
+++ lib/features/dashboard/models/country_model.dart
@@ -0,0 +1,24 @@
+enum Continent { afrika, asia, eropa, as }
+
+class CountryOrder {
+  final String id;
+  final String name;
+  final String flagAsset;
+  final Continent continent;
+  final double unlockCost;
+  final double upgradeCost;
+  final Duration deliveryTime;
+  
+  bool isUnlocked;
+  int level;
+  List<OrderItem> requiredItems;
+  int rewardKeyMin;
+  int rewardKeyMax;
+
+  CountryOrder({
+    required this.id, required this.name, required this.flagAsset,
+    required this.continent, required this.unlockCost, required this.upgradeCost,
+    required this.deliveryTime, this.isUnlocked = false, this.level = 1,
+    this.requiredItems = const [], this.rewardKeyMin = 1, this.rewardKeyMax = 10,
+  });
+}
