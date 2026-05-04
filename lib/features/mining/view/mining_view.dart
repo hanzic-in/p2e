07d3c2c0603a8bc2f9e6cce39921cff85b10e19d
Han <rizkyupit40@gmail.com>
2026-05-04 11:02:51 +0700

    Update mining_view.dart

--- lib/features/mining/view/mining_view.dart
+++ lib/features/mining/view/mining_view.dart
@@ -38,8 +38,7 @@ class _MiningViewState extends State<MiningView> with SingleTickerProviderStateM
     final tokenColor = const Color(0xFF00E5FF); 
     final boostColor = const Color(0xFFC154F7);
     final activeThemeColor = prov.isBoostActive ? boostColor : tokenColor;
-    prov.refreshMining();
-    prov.refreshBoost();
+    prov.refreshAll();
 
     return Scaffold(
       backgroundColor: const Color(0xFF0F1116),
