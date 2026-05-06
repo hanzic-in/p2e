2b1d3b67de79f6dfe767c5526ab37aec1d5784a6
Han <rizkyupit40@gmail.com>
2026-05-06 17:52:09 +0700

    Update build.gradle.kts

--- android/app/build.gradle.kts
+++ android/app/build.gradle.kts
@@ -6,7 +6,7 @@ plugins {
 }
 
 android {
-    namespace = "com.example.discire_app"
+    namespace = "com.example.greenharvest"
     compileSdk = flutter.compileSdkVersion
     ndkVersion = flutter.ndkVersion
 
